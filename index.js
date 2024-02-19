24ea1a4234783618b36418a91c649b3dd8285d58
jsf <azimut.nt@gmail.com>
2024-02-19 13:07:33 +0700

    wss

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
 import { identify } from '@libp2p/identify'
 import { webSockets } from '@libp2p/websockets'
 import { createLibp2p } from 'libp2p'
+import { wss } from '@libp2p/websockets/filters'
 
 const node = await createLibp2p({
     addresses: {
-        listen: ['/ip4/0.0.0.0/tcp/4001/ws']
+        listen: ['/ip4/0.0.0.0/tcp/4001/wss']
     },
     transports: [
-        webSockets()
+        webSockets({
+            filter: wss
+        })
     ],
     connectionEncryption: [
         noise()
